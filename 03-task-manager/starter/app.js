996badc3917af8723e7ced20abc5df723d7026de
Devblaise <blaisembadugha@gmail.com>
2021-08-14 09:09:27 -0700

    07-file-upload after recording
    	modified:   07-file-upload/final/controllers/uploadsController.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,30 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+	res.send("welcome to task manager project");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, () => {
+			console.log(`app is listening on port ${port}...`);
+		});
+	} catch (err) {
+		console.log(err.message);
+	}
+};
+
+start();
